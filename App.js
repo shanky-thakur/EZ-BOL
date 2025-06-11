47c36f48bcceaeafc0b5ce5a987f3482caf83a99
shanky <128723278+shanky-thakur@users.noreply.github.com>
2025-06-11 21:16:16 +0530

    landing page done

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Image, StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useRef, useEffect } from 'react';
 
 export default function App() {
+  const scaleAnimation = useRef(new Animated.Value(0.5)).current;
+
+  useEffect(() => {
+    Animated.timing(scaleAnimation, {
+      toValue: 1,
+      duration: 1500,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      {/* status bar layout */}
+      <StatusBar style='dark' ></StatusBar>
+      <View style={styles.statusBar}></View>
+
+      {/* main body content */}
+      <View style={styles.body}>
+        <LinearGradient style={styles.gradientContainer}
+          colors={['#ffffff', '#f8f8ff']}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 1 }}
+        >
+          <View style={styles.gradientContainerBody}>
+            <Animated.Image
+              source={require('./assets/EZ_BOL-removebg-preview.png')}
+              style={{ transform: [{ scale: scaleAnimation }] }}
+              resizeMode="cover"></Animated.Image>
+          </View>
+        </LinearGradient>
+      </View>
+
+      {/* android naviagtion layout */}
+      <View style={styles.bottom}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  statusBar: {
+    width: '100%',
+    height: '4.5%',
+  },
+  body: {
+    width: '100%',
+    height: '90%'
+  },
+  gradientContainer: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottom: {
+    width: '100%',
+    height: '5.5%',
+  },
+  gradientContainerBody: {
+    width: '55%',
+    height: '25%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
